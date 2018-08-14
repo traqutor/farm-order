201ca12d383b146e04d09117e0a81291526fab8f
Bartlomiej Mikolajczuk <b.mikolajczuk@blunovation.com>
2018-08-14 12:20:48 +0200

    frontend changes

--- frontend/src/app/core/auth/auth.service.ts
+++ frontend/src/app/core/auth/auth.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
+interface AuthResponse {
+  '.expires': string;
+  '.issued': string;
+  access_token: string;
+  expires_in: number;
+  token_type: string;
+  userName: string;
+}
+
 export class AuthService {
 
-  constructor() { }
+  apiUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.url;
+  }
+
+  login(credentials): void {
+    const {login, password} = credentials;
+    const body = new HttpParams()
+      .set('grant_type', 'password')
+      .set('userName', login)
+      .set('password', password);
+    this.http.get(`${this.apiUrl}/token`)
+  }
+
 }
