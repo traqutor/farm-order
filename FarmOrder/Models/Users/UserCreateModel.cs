0e779624e1979be2216d5ead4445fd2c9ad069c4
Kamil Skar <kamilskar@gmail.com>
2018-08-17 12:49:13 +0200

    user create validation fix.

--- FarmOrder/Models/Users/UserCreateModel.cs
+++ FarmOrder/Models/Users/UserCreateModel.cs
@@ -15,7 +15,7 @@ namespace FarmOrder.Models.Users
         public string Id { get; set; }
 
         [Required]
-        [StringLength(1, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         public string UserName { get; set; }
 
         /// <summary>
