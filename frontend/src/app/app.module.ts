ff5f0cf0aa7051d64859975b99cba42f734b9038
Mariusz Augustyniak <maugustyniak@gmail.com>
2018-09-24 11:40:03 +0200

    Date format

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {MomentDateAdapter} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppMaterialModule } from './app-material.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core/core.module';
-import { OrdersModule } from './orders/orders.module';
-import { UsersModule } from './users/users.module';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './core/auth/auth.interceptor';
-import { ConfirmDialogComponent } from './shared/dialogs/confirm-dialog/confirm-dialog.component';
-import { ChangePasswordDialogComponent } from './shared/dialogs/change-password-dialog/change-password-dialog.component';
-import { AlertDialogComponent } from './shared/dialogs/alert-dialog/alert-dialog.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppMaterialModule} from './app-material.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SharedModule} from './shared/shared.module';
+import {CoreModule} from './core/core.module';
+import {OrdersModule} from './orders/orders.module';
+import {UsersModule} from './users/users.module';
+import {AuthInterceptor} from './core/auth/auth.interceptor';
+import {ConfirmDialogComponent} from './shared/dialogs/confirm-dialog/confirm-dialog.component';
+import {ChangePasswordDialogComponent} from './shared/dialogs/change-password-dialog/change-password-dialog.component';
+import {AlertDialogComponent} from './shared/dialogs/alert-dialog/alert-dialog.component';
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'DD/MM/YYYY',
+    monthYearA11yLabel: 'MMM YYYY',
+  },
+};
+
 
 @NgModule({
   declarations: [
@@ -30,7 +45,9 @@ import { AlertDialogComponent } from './shared/dialogs/alert-dialog/alert-dialog
     UsersModule,
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
   ],
   entryComponents: [
     ConfirmDialogComponent,
