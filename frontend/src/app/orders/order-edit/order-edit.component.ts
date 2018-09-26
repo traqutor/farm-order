6ff27c5799a1fe9b933dd4fc2818f04fb03d18d0
Mariusz Augustyniak <maugustyniak@gmail.com>
2018-09-26 20:30:13 +0200

    Order Add/Edit Form validation

--- frontend/src/app/orders/order-edit/order-edit.component.ts
+++ frontend/src/app/orders/order-edit/order-edit.component.ts
@@ -8,7 +8,7 @@ import {Observable} from 'rxjs';
 import {SharedService} from '../../shared/shared.service';
 import {OrdersService} from '../orders.service';
 import {Farm} from '../../shared/models/farm';
-import {OrderChangeReason, Status} from '../../shared/models/order';
+import {IOrder, OrderChangeReason, Status} from '../../shared/models/order';
 import {DialogService} from '../../shared/dialogs/dialog.service';
 import {Ration} from '../../shared/models/ration';
 import {ISilo} from "../../shared/models/silo";
@@ -31,15 +31,21 @@ export class OrderEditComponent implements OnInit {
               private dialogService: DialogService) {
   }
 
+
   order: FormGroup;
   farms$: Observable<{ results: Array<Farm>, resultsCount: number }>;
   rations$: Observable<{ results: Array<Ration>, resultsCount: number }>;
   status$: Observable<{ results: Array<Status>, resultsCount: number }>;
+
+  farms: { results: Array<Farm>, resultsCount: number };
   sheds$: Observable<{ results: [IShed], resultsCount: number }>;
+  sheds: { results: [IShed], resultsCount: number };
   silos$: Observable<{ results: [ISilo], resultsCount: number }>;
+  silos: { results: [ISilo], resultsCount: number };
 
   orderChangeReason$: Observable<{ results: Array<OrderChangeReason>, resultsCount: number }>;
   orderId;
+  orderTmp: IOrder;
   orderTotalTonnage: number = 0;
   orderSilosTonnage: number = 0;
 
@@ -50,44 +56,46 @@ export class OrderEditComponent implements OnInit {
         this.orderId = +params['id'];
         this.ordersService.getOrderById(this.orderId)
           .subscribe(order => {
-
+            this.orderTmp = order;
             this.orderTotalTonnage = order.tonsOrdered;
 
             this.order = this.fb.group({
-              tonsOrdered: [order.tonsOrdered, [
+              tonsOrdered: [this.orderTmp.tonsOrdered, [
                 Validators.required,
                 Validators.min(1),
               ]],
-              deliveryDate: [order.deliveryDate, [
+              deliveryDate: [this.orderTmp.deliveryDate, [
                 Validators.required
               ]],
-              farm: [order.farm, [
+              farm: [this.orderTmp.farm, [
                 Validators.required
               ]],
-              ration: [order.ration, [
+              ration: [this.orderTmp.ration, [
                 Validators.required
               ]],
-              sheds: [order.sheds, [
+              sheds: [this.orderTmp.sheds, [
                 Validators.required
               ]],
-              silos: [order.silos, [
+              silos: [this.orderTmp.silos, [
                 Validators.required
               ]],
-              status: [order.status, [
+              status: [this.orderTmp.status, [
                 Validators.required
               ]],
-              orderChangeReason: [order.orderChangeReason, [
+              orderChangeReason: [this.orderTmp.orderChangeReason, [
                 Validators.required
               ]]
             });
 
-            this.getRations(order.farm);
+            this.getRations(this.orderTmp.farm);
+            this.getFarms();
 
-            this.sheds$ = this.sharedService.getSheds(order.farm.id, null);
+
+            this.sheds$ = this.sharedService.getSheds(this.orderTmp.farm.id, null);
             this.sheds$.subscribe((res: { results: Array<IShed>, resultsCount: number }) => {
               let tmp: Array<IShed> = [];
               res.results.forEach((shed: IShed) => {
-                order.silos.forEach((silo: ISilo) => {
+                this.orderTmp.silos.forEach((silo: ISilo) => {
                   if (shed.id === silo.shedId) {
                     tmp.push(shed);
                   }
@@ -107,6 +115,15 @@ export class OrderEditComponent implements OnInit {
     this.status$ = this.sharedService.getStatus();
     this.orderChangeReason$ = this.sharedService.getOrderChangeReason();
   }
+
+  getFarms() {
+    this.sharedService.getUserAssignedFarms(null)
+      .subscribe((farms: { results: Array<Farm>, resultsCount: number }) => {
+        this.farms = farms;
+      });
+
+  }
+
 
   compare(val1, val2) {
     if (val1 && val2) {
@@ -129,29 +146,61 @@ export class OrderEditComponent implements OnInit {
   }
 
   recalculateOrderTonnage() {
-    'aaaaa'
-    this.orderSilosTonnage = 0;
     if (this.order) {
+      this.orderSilosTonnage = 0;
       this.order.controls.silos.value.forEach((silo: ISilo) => {
         this.orderSilosTonnage = this.orderSilosTonnage + silo.amount;
       });
     }
   }
 
-  getSilos(sheds: Array<IShed>) {
-    let tmp: Array<ISilo> = [];
-    if (this.order) {
-      sheds.forEach((shed: IShed) => {
-        this.order.controls.silos.value.forEach((silo: ISilo) => {
-          if (shed.id === silo.shedId) {
-            tmp.push(silo);
+  onSiloSelectChange() {
+    setTimeout(()=> {
+      this.orderTmp.silos.forEach((silo: ISilo) => {
+        this.order.controls.silos.value.forEach((siloTmp: ISilo) => {
+          if (siloTmp.id === silo.id) {
+            siloTmp.amount = silo.amount;
           }
         });
       });
-      this.order.controls.silos.setValue(tmp);
-    }
+      this.recalculateOrderTonnage();
+    }, 300);
+  }
+
+  onSiloAmountChange(silo: ISilo) {
+    this.orderTmp.silos.forEach((siloTmp: ISilo) => {
+      if (siloTmp.id === silo.id) {
+        siloTmp.amount = silo.amount;
+      }
+    });
     this.recalculateOrderTonnage();
+  }
+
+  getSilos(sheds: Array<IShed>) {
+
+    this.orderSilosTonnage = 0;
     this.silos$ = this.sharedService.getSilos(sheds, null);
+    this.silos$.subscribe((res: { results: Array<ISilo>, resultsCount: number }) => {
+
+      let tmp: Array<ISilo> = [];
+
+      if (this.order) {
+
+        this.order.controls.silos.value.forEach((silo: ISilo) => {
+          sheds.forEach((shed: IShed) => {
+            if (shed.id === silo.shedId) {
+              tmp.push(silo);
+            }
+          });
+        });
+
+        this.order.controls.silos.setValue(tmp);
+
+        this.onSiloSelectChange();
+
+      }
+
+    });
   }
 
   onSubmit() {
